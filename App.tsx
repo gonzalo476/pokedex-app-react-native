389ada932fc052e9b38d25b7b2ebe7fa8ba7cc65
GONZALO ROJAS <rojasg476@gmail.com>
2021-12-01 13:29:29 -0600

    React Native Setup

---
+++ App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react'
+import {View, Text} from 'react-native'
+
+function App() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default App
