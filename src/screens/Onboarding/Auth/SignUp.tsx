b116ac3c28447bae3db5b3c2f4d60e7bd2ac81e0
GONZALO ROJAS <rojasg476@gmail.com>
2021-12-02 20:34:50 -0600

    sign up text

--- src/screens/Onboarding/Auth/SignUp.tsx
+++ src/screens/Onboarding/Auth/SignUp.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {StyleSheet, View} from 'react-native'
 
-import {Colors, Header} from '../../../components'
+import {Colors, Header, Text} from '../../../components'
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.blueDark,
     flex: 1,
+  },
+  content: {
+    paddingHorizontal: 16,
   },
 })
 
@@ -14,6 +17,11 @@ const SignUp = () => {
   return (
     <View style={styles.container}>
       <Header arrowLeft onPressArrowLeft={() => console.log('goBack')} />
+      <View style={styles.content}>
+        <Text variant="largeTitle" type="Bold" marginV="l">
+          Crear Cuenta
+        </Text>
+      </View>
     </View>
   )
 }
